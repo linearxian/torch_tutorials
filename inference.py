81aea6ef83696dbb7dd092ea69f26b00d0a39bfe
linearxian <endloswu@gmail.com>
2021-08-18 17:35:02 +0200

    added plotting

--- inference.py
+++ inference.py
@@ -1,5 +1,7 @@
 import torch
 from train import FeedForwardNet, download_mnist_datasets
+from torch.utils.data import DataLoader
+import matplotlib.pyplot as plt
 
 
 class_mapping = [
@@ -38,8 +40,17 @@ if __name__ == "__main__":
     _, validation_data = download_mnist_datasets()
 
     # get a sample from the validation dataset for inference
-    input, target = validation_data[0][0], validation_data[0][1]
+    # input, target = validation_data[0][0], validation_data[0][1]
+    validation_data_loader = DataLoader(validation_data, batch_size=10)
 
-    # make an inference
-    predicted, expected = predict(feed_forward_net, input, target, class_mapping)
-    print(f"Predicted: '{predicted}', Expected: '{expected}'")
+    i = 0
+    while i<10:
+        input, target = next(iter(validation_data_loader))
+        # make an inference
+        predicted, expected = predict(feed_forward_net, input[i], target[i], class_mapping)
+        print(f"Predicted: '{predicted}', Expected: '{expected}'")
+        plt.figure()
+        plt.imshow(input[i].reshape(28,28), cmap="gray")
+        plt.title(f"Predicted: '{predicted}', Expected: '{expected}'")
+        plt.show()
+        i = i + 1
